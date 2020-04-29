452b5fa2379c9bc081c3deef7a60fe06f46c26d5
bootslee <410141599@qq.com>
2020-04-30 01:08:50 +0800

    202. 快乐数

---
+++ src/main/java/com/bootslee/leetcode/HappyNumber202.java
@@ -0,0 +1,66 @@
+package com.bootslee.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber202 {
+    private static Set<Integer> cycleMembers =
+            new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    /**
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     * 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，
+     * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+     * 如果 可以变为  1，那么这个数就是快乐数。
+     * 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+    public boolean isHappy3(int n) {
+        while(n!=1&&n!=4){
+            int sum=0;
+            while(n>0){
+                sum += (n%10)*(n%10);
+                n /=10;
+            }
+            n=sum;
+        }
+        return n==1;
+    }
+    public boolean isHappy2(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    public boolean isHappyByFastSlow(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+
+}
